5e58c2236a3277879277d583e223b8651aa17acc
Monica Tamaru Jones <mntamaru@gmail.com>
2018-10-08 16:40:49 +0000

    automatically backed up by learn

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,11 @@
 // Code Keypad Component Here
+import react, { Component} from 'react';
+
+export default class Keypad extends Component {
+
+  render() {
+    return <input type="password" onkeyUp={
+      console.log("Entering password...")
+    } />
+  }
+}
